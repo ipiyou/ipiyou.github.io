d9cdc0335fdc4a4a1df25ac1e7ea732bc3be88cc
ipiyou <tao710803@gmail.com>
2023-04-02 01:39:12 +0900

    fix: theme 설정 변경,d.ts 추가

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -3,6 +3,8 @@ export const theme = {
     red: "#FF0000",
     blue: "#1400FF",
     green: "#00FF19",
+    black: "#000000",
+    gray: "#858585",
   },
   radius: {
     small: "4px",
@@ -12,9 +14,9 @@ export const theme = {
   shadow: {
     item: "0 0 10px 0 rgba(0, 0, 0, 0.25)",
   },
-};
+} as const;
 
 type theme = typeof theme;
-export type keyOfColor = theme["color"];
-export type keyofRadius = theme["radius"];
-export type keyofShadow = theme["shadow"];
+export type keyOfColor = keyof theme["color"];
+export type keyofRadius = keyof theme["radius"];
+export type keyofShadow = keyof theme["shadow"];
