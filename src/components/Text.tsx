54ca4b60b709b1516c8e7319c24ec3a9959f10ca
ipiyou <tao710803@gmail.com>
2023-04-02 01:39:55 +0900

    feat: 텍스트 컴포넌트 추가

---
+++ src/components/Text.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { keyOfColor } from "../style/theme";
+
+export const _Text = styled.div<{ color: keyOfColor; size: string }>`
+  color: ${({ theme, color }) => theme.color[color]};
+  size: ${({ size }) => size};
+`;
